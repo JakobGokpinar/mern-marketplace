77dd961131a0ad22f67d9a456f2a09d6b94c2593
jakobgokpinar <jakobgokpinar@gmail.com>
2026-02-26 16:05:46 +0100

    UX redesign: index.js update

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import reportWebVitals from './reportWebVitals';
-
 import App from './App.js';
 import store from './config/store-config.js';
-import './design.css'
-
+import './design.css';
 import process from 'process';
 window.process = process;
 
 ReactDOM.render(
   <Provider store={store}>
-     <App></App>
-  </Provider>
-  ,document.getElementById('root') 
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
